80e6dea9b3f9064ea65774a5f9b2ccb8c234ff31
Victor Cruz <motoggb979@gmail.com>
2022-07-24 19:08:19 -0300

    feat: criando HTML react da tela inicio

---
+++ src/components/Inicio/index.jsx
@@ -0,0 +1,17 @@
+import "./style.css";
+
+const Inicio = () => {
+  return (
+    <div className="container__inicio">
+      <div>
+        <img src="./assets/nuKenzie.png" alt="" />
+        <h1>Centralize o controle das suas finanças</h1>
+        <p>de forma rápida e segura</p>
+        <button>Iniciar</button>
+      </div>
+      <img src="./assets/nuKenzie-inicio.svg" alt="" />
+    </div>
+  );
+};
+
+export default Inicio;
